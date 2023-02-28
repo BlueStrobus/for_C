e06f38a3fbbcb3753ee3d40b3f21fd53f618daca
strobus <116132298+BlueStrobus@users.noreply.github.com>
2023-02-28 11:41:34 +0900

    Create Ex055.cs

---
+++ Csharp/RoadBook.CsharpBasic.Chapter03/MyfirstApp02/Exemples/Ex055.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyfirstApp02.Exemples
+{
+    public class Ex055
+    {
+        public void Run()
+        {
+            Sum(1, 1);
+            Sum(2, 2);
+            Sum(3, 3);
+            Sum(8, 32);
+        }
+
+        private void Sum(int x, int y)
+        {
+            Console.WriteLine("{0} + {1} = {2}", x, y, x + y);
+        }
+    }
+}
